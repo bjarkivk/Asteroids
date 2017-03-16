24fcc86ca43e14214a616004b270b208cc53502b
bjarkivk <bjarkivk95@gmail.com>
2017-03-16 09:04:49 +0000

    Add files via upload

---
+++ asteroids.js
@@ -0,0 +1,193 @@
+/////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//     Jörðin (sem teningur!) snýst um sólina (stærri teningur!)
+//
+//    Hjálmtýr Hafsteinsson, febrúar 2017
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+var numVertices  = 36;
+
+var points = [];
+var colors = [];
+
+var movement = false;     // Do we rotate?
+var spinX = 0;
+var spinY = 0;
+var origX;
+var origY;
+
+var rotYear = 0.0;
+var rotDay = 0.0;
+var rotMonth = 0.0;
+var rotMonth2 = 0.0;
+var earthTilt = 23.5;
+
+var matrixLoc;
+
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    colorCube();
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    matrixLoc = gl.getUniformLocation( program, "rotation" );
+
+    //event listeners for mouse
+    canvas.addEventListener("mousedown", function(e){
+        movement = true;
+        origX = e.offsetX;
+        origY = e.offsetY;
+        e.preventDefault();         // Disable drag and drop
+    } );
+
+    canvas.addEventListener("mouseup", function(e){
+        movement = false;
+    } );
+
+    canvas.addEventListener("mousemove", function(e){
+        if(movement) {
+    	    spinY = ( spinY + (e.offsetX - origX) ) % 360;
+            spinX = ( spinX + (e.offsetY - origY) ) % 360;
+            origX = e.offsetX;
+            origY = e.offsetY;
+        }
+    } );
+
+    render();
+}
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d)
+{
+    var vertices = [
+        vec3( -0.5, -0.5,  0.5 ),
+        vec3( -0.5,  0.5,  0.5 ),
+        vec3(  0.5,  0.5,  0.5 ),
+        vec3(  0.5, -0.5,  0.5 ),
+        vec3( -0.5, -0.5, -0.5 ),
+        vec3( -0.5,  0.5, -0.5 ),
+        vec3(  0.5,  0.5, -0.5 ),
+        vec3(  0.5, -0.5, -0.5 )
+    ];
+
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+
+    //vertex color assigned by the index of the vertex
+
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        colors.push(vertexColors[indices[i]]);
+
+    }
+}
+
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    rotDay += 3.0;
+    rotYear += 0.1;        // not the correct value, but looks better!
+    rotMonth += 1.0;
+    rotMonth2 += 1.2;
+
+    var ctm = mat4();
+    ctm = mult( ctm, rotateX(spinX) );
+    ctm = mult( ctm, rotateY(spinY) );
+
+    var ctm1 = mat4();
+    ctm1 = mult( ctm1, rotateX(spinX) );
+    ctm1 = mult( ctm1, rotateY(spinY) );
+
+    // teikna "sólina"
+    ctm = mult( ctm, scalem( 0.3, 0.3, 0.3 ) );
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm));
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+
+    // teikna "jörðina"
+    /*ctm = mult( ctm, rotateY( rotYear ) );
+    ctm = mult( ctm, translate( 2.0, 0.0, 0.0 ) );
+    ctm = mult( ctm, rotateZ( earthTilt ) );
+    ctm = mult( ctm, rotateY( rotDay ) );
+
+    ctm = mult( ctm, scalem( 0.2, 0.2, 0.2 ) );
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm));
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );*/
+
+    // teikna tungl 1
+    /*ctm1 = mult( ctm, rotateY( rotMonth ) );
+    ctm1 = mult( ctm1, translate( 2.0, 0.0, 0.0 ) );
+
+    ctm1 = mult( ctm1, scalem( 0.5, 0.5, 0.5 ) );
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm1));
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );*/
+
+    // teikna tungl 2
+
+    /*ctm2 = mult( ctm, rotateY( rotMonth2 ) );
+    ctm2 = mult( ctm2, rotateZ( earthTilt ) );
+    ctm2 = mult( ctm2, translate( 3.0, 0.0, 0.0 ) );
+    ctm2 = mult( ctm2, scalem( 0.5, 0.5, 0.5 ) );
+    ctm2 = mult( ctm2, rotateY( rotDay ));
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(ctm2));
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );*/
+
+    requestAnimFrame( render );
+}
